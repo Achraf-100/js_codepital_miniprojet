55f3a26d8ddd5700d3b9d431bbd36c390fbba757
Achraf <hardy1348@live.fr>
2021-01-26 09:27:22 +0100

    fin2

--- public/js/function/function.js
+++ public/js/function/function.js
@@ -1,140 +1,57 @@
 import {marc,docteur, optim, goku, vader, colon} from "../main.js";
+import {tab} from "../main.js";
 import {porteMonnaie} from "../objeet/objet.js";
 
-export let traitement = () =>{
-    if (marc.maladie == 'Mal indente') {
-        console.log(`Le traitement pour ${marc.nom} est ctrl+maj+f`);
-    }
-}
-export let traitement2 = () =>{
-    if (optim.maladie == 'Unsave') {
-        console.log(`Le traitement pour ${optim.nom} est saveOnFocusChange`);
-    }
-}
-export let traitement3 = () =>{
-    if (goku.maladie == '404') {
-        console.log(`Le traitement pour ${goku.nom} est CheckLinkRelation`);
-    }
-}
-export let traitement4 = () =>{
-    if (vader.maladie == 'azmatique') {
-        console.log(`Le traitement pour ${vader.nom} est Ventoline`);
-    }
-}
-export let traitement5 = () =>{
-    if (colon.maladie == 'syntaxError') {
-        console.log(`Le traitement pour ${colon.nom} est f12+doc`);
-    }
+
+export let traitement = (a) =>{
+    tab.forEach(element => {
+        if (a.maladie == 'Mal indente' && a.maladie == element.maladie) {
+            console.log(`Le traitement pour ${a.nom} est ctrl+maj+f`);
+        }else if (a.maladie == 'Unsave'&& a.maladie == element.maladie) {
+            console.log(`Le traitement pour ${a.nom} est saveOnFocusChange`);
+        }else if (a.maladie == '404'&& a.maladie == element.maladie) {
+            console.log(`Le traitement pour ${a.nom} est CheckLinkRelation`);
+        }else if (a.maladie == 'azmatique'&& a.maladie == element.maladie) {
+            console.log(`Le traitement pour ${a.nom} est Ventoline`);
+        }else if (a.maladie == 'syntaxError'&& a.maladie == element.maladie) {
+            console.log(`Le traitement pour ${a.nom} est f12+doc`);
+        }
+    });
 }
 
 
-
-export let payement = () => {
-    if (marc.maladie == 'Mal indente') {
-        let resultat = marc.argent - porteMonnaie.tarif
-        let resultat1 = marc.argent - porteMonnaie.tarif
-        marc.argent = resultat1
-        porteMonnaie.argent = resultat
-        console.log(`${marc.nom} à payé le ${docteur.nom} ${porteMonnaie.tarif}`)
-    }
-}
-export let payement1 = () => {
-    if (optim.maladie == 'Unsave') {
-        let resultat = optim.argent - porteMonnaie.tarif
-        let resultat1 = optim.argent - porteMonnaie.tarif
-        optim.argent = resultat1
-        porteMonnaie.argent = resultat
-        console.log(`${optim.nom} à payé le ${docteur.nom} ${porteMonnaie.tarif}`)
-    }
-}
-export let payement2 = () => {
-    if (goku.maladie == '404') {
-        let resultat = goku.argent - porteMonnaie.tarif
-        let resultat1 = goku.argent - porteMonnaie.tarif
-        goku.argent = resultat1
-        porteMonnaie.argent = resultat
-        console.log(`${goku.nom} à payé le ${docteur.nom} ${porteMonnaie.tarif}`)
-    }
-}
-export let payement3 = () => {
-    if (vader.maladie == 'azmatique') {
-        let resultat = vader.argent - porteMonnaie.tarif
-        let resultat1 = vader.argent - porteMonnaie.tarif
-        vader.argent = resultat1
-        porteMonnaie.argent = resultat
-        console.log(`${vader.nom} à payé le ${docteur.nom} ${porteMonnaie.tarif}`)
-    }
-}
-export let payement4 = () => {
-    if (colon.maladie == 'syntaxError') {
-        let resultat = colon.argent - porteMonnaie.tarif
-        let resultat1 = colon.argent - porteMonnaie.tarif
-        colon.argent = resultat1
-        porteMonnaie.argent = resultat
-        console.log(`${colon.nom} à payé le ${docteur.nom} ${porteMonnaie.tarif}`)
-    }
+export let payement = (a) =>{
+    tab.forEach(Element => {
+        if (a.nom == Element.nom) {
+            let resultat = a.argent - porteMonnaie.tarif
+            let resultat1 = a.argent - porteMonnaie.tarif
+            a.argent = resultat1
+            porteMonnaie.argent = resultat
+            console.log(`${a.nom} à payé le ${docteur.nom} ${porteMonnaie.tarif}`)
+        }
+    });
 }
 
 
-
-
-export let payementPharmacie = () => {
-    if (marc.argent < 60) {
-        console.log(`${marc.nom} n'a pas assez pour payer son traitement`);
-    }
-}
-export let payementPharmacie1 = () => {
-    if (optim.argent > 60) {
-        console.log(`${optim.nom} a assez pour payer son traitement`);
-    }
-}
-export let payementPharmacie2 = () => {
-    if (goku.argent < 60) {
-        console.log(`${goku.nom} n'a pas assez pour payer son traitement`);
-    }
-}
-export let payementPharmacie3 = () => {
-    if (vader.argent >= 60) {
-        console.log(`${vader.nom} a assez pour payer son traitement`);
-    }
-}
-export let payementPharmacie4 = () => {
-    if (colon.argent < 60) {
-        console.log(`${colon.nom} n'a pas assez pour payer son traitement`);
-    }
+export let payementPharmacie = (a) => {
+    tab.forEach(element => {
+        if (a.argent < 60 && a.nom == element.nom) {
+            console.log(`${a.nom} n'a pas assez pour payer son traitement`);
+        }else if (a.argent >= 60 && a.nom == element.nom){
+            console.log(`${a.nom} a assez pour payer son traitement`);
+        }
+    });
 }
 
 
-
-
-export let jugement = () => {
-    if (marc.etatSante == 'malade') {
-        marc.etatSante = 'mort'
-        console.log(`${marc.nom} est ${marc.etatSante}`);
-    }
+export let jugement = (a) => {
+    tab.forEach(element => {
+        if (a.argent < 0 && a.nom == element.nom) {
+            a.etatSante = 'mort'
+            console.log(`${a.nom} est ${a.etatSante}`);
+        }else if (a.argent >= 0 && a.nom == element.nom) {
+            a.etatSante = 'vivant'
+            console.log(`${a.nom} est ${a.etatSante}`);
+        }
+    });
 }
-export let jugement1 = () => {
-    if (optim.etatSante == 'malade') {
-        optim.etatSante = 'Vivant'
-        console.log(`${optim.nom} est ${optim.etatSante}`);
-    }
-}
-export let jugement2 = () => {
-    if (goku.etatSante == 'malade') {
-        goku.etatSante = 'mort'
-        console.log(`${goku.nom} est ${goku.etatSante}`);
-    }
-}
-export let jugement3 = () => {
-    if (vader.etatSante == 'malade') {
-        vader.etatSante = 'vivant'
-        console.log(`${vader.nom} est ${vader.etatSante}`);
-    }
-}
-export let jugement4 = () => {
-    if (colon.etatSante == 'malade') {
-        colon.etatSante = 'mort'
-        console.log(`${colon.nom} est ${colon.etatSante}`);
-    }
-}
-
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,6 @@
 import {Marcus,Optimus,Sangoku,DarthVader,Semicolon,Debugger} from "./class/class.js"
 import {cimetiere,lieu,lieu2,lieu3,porteMonnaie} from "./objeet/objet.js";
-import {traitement,payement,payementPharmacie, jugement,traitement2,
-    traitement3,traitement4,traitement5,payement1,payement2,payement3,payement4,
-    payementPharmacie1,payementPharmacie2,payementPharmacie3,payementPharmacie4,
-    jugement1,jugement2,jugement3,jugement4} from "./function/function.js";
+import {traitement,payement,payementPharmacie, jugement} from "./function/function.js";
 
 export let marc = new Marcus()
 export let optim = new Optimus()
@@ -13,7 +10,15 @@ export let colon = new Semicolon()
 export let docteur = new Debugger()
 lieu.deplacement()
 
+export let tab = []
+tab.push(marc)
+tab.push(optim)
+tab.push(goku)
+tab.push(vader)
+tab.push(colon)
+console.log(tab);
 // Marcus
+
 
 console.log(`Dans la salle d'attente il y'a donc ${lieu.personnes.length} personnes`);
 console.log('_______________');
@@ -44,16 +49,16 @@ optim.goTo(lieu3)
 lieu3.welcome(optim)
 console.log(`Dans la salle d'attente il y'a donc ${lieu.personnes.length} personnes`);
 console.log(`La maladie de ${optim.nom} est ${optim.maladie}`);
-traitement2(optim)
-payement1(optim)
+traitement(optim)
+payement(optim)
 console.log(`${optim.nom} a actuellement ${optim.argent}`);
 console.log(`${optim.nom} a quitté le ${lieu3.nom}`);
 lieu2.goToo(marc)
 // console.log(lieu2.personnes);
 console.log(`${optim.nom} est allé à la ${lieu2.nom}`);
 console.log(`Le traitement de ${optim.nom} à la ${lieu2.nom} coute ${lieu2.traitementMarcus}`);
-payementPharmacie1(optim)
-jugement1(optim)
+payementPharmacie(optim)
+jugement(optim)
 console.log(`${optim.nom} est ${optim.etatSante}, donc il est au Iles Canaries`);
 // console.log(cimetiere.personnes);
 
@@ -65,16 +70,16 @@ goku.goTo(lieu3)
 lieu3.welcome(goku)
 console.log(`Dans la salle d'attente il y'a donc ${lieu.personnes.length} personnes`);
 console.log(`La maladie de ${goku.nom} est ${goku.maladie}`);
-traitement3(goku)
-payement2(goku)
+traitement(goku)
+payement(goku)
 console.log(`${goku.nom} a actuellement ${goku.argent}`);
 console.log(`${goku.nom} a quitté le ${lieu3.nom}`);
 lieu2.goToo(goku)
 // console.log(lieu2.personnes);
 console.log(`${goku.nom} est allé à la ${lieu2.nom}`);
 console.log(`Le traitement de ${goku.nom} à la ${lieu2.nom} coute ${lieu2.traitementMarcus}`);
-payementPharmacie2(goku)
-jugement2(goku)
+payementPharmacie(goku)
+jugement(goku)
 console.log(`${goku.nom} est mort, donc il est au ${cimetiere.nom}`);
 cimetiere.dead(goku)
 // console.log(cimetiere.personnes);
@@ -88,16 +93,17 @@ vader.goTo(lieu3)
 lieu3.welcome(vader)
 console.log(`Dans la salle d'attente il y'a donc ${lieu.personnes.length} personnes`);
 console.log(`La maladie de ${vader.nom} est ${vader.maladie}`);
-traitement4(vader)
-payement3(vader)
+traitement(vader)
+payement(vader)
+console.log(vader.argent);
 console.log(`${vader.nom} a actuellement ${vader.argent}`);
 console.log(`${vader.nom} a quitté le ${lieu3.nom}`);
-lieu2.goToo(marc)
+lieu2.goToo(vader)
 // console.log(lieu2.personnes);
 console.log(`${vader.nom} est allé à la ${lieu2.nom}`);
 console.log(`Le traitement de ${vader.nom} à la ${lieu2.nom} coute ${lieu2.traitementMarcus}`);
-payementPharmacie3(vader)
-jugement3(vader)
+payementPharmacie(vader)
+jugement(vader)
 console.log(`${vader.nom} est ${vader.etatSante}, donc il est au Iles Canaries`);
 // console.log(cimetiere.personnes);
 
@@ -109,16 +115,16 @@ colon.goTo(lieu3)
 lieu3.welcome(colon)
 console.log(`Dans la salle d'attente il y'a donc ${lieu.personnes.length} personnes`);
 console.log(`La maladie de ${colon.nom} est ${colon.maladie}`);
-traitement5(colon)
-payement4(colon)
+traitement(colon)
+payement(colon)
 console.log(`${colon.nom} a actuellement ${colon.argent}`);
 console.log(`${colon.nom} a quitté le ${lieu3.nom}`);
 lieu2.goToo(colon)
 // console.log(lieu2.personnes);
 console.log(`${colon.nom} est allé à la ${lieu2.nom}`);
 console.log(`Le traitement de ${colon.nom} à la ${lieu2.nom} coute ${lieu2.traitementMarcus}`);
-payementPharmacie4(colon)
-jugement4(colon)
+payementPharmacie(colon)
+jugement(colon)
 console.log(`${colon.nom} est mort, donc il est au ${cimetiere.nom}`);
 cimetiere.dead(colon)
 // console.log(cimetiere.personnes);
